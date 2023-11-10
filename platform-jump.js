2379042e732b0f81a585ee84a60d16c174aad7d8
Kacper <kacpercichy@onet.pl>
2023-11-10 01:42:38 +0100

    small fixes

--- platform-jump.js
+++ platform-jump.js
@@ -2,14 +2,12 @@ import { initializeBoard} from './board.js';
 import {moveBunny, placePlatforms, stopBunny, update} from "./game.js";
 import {drawBunny} from "./bunny.js";
 import {drawPlatforms} from "./platforms.js";
-import {setBaseY} from "./physics.js";
 
 
 window.onload = function () {
     initializeBoard();
     drawBunny();
     drawPlatforms();
-    setBaseY();
     placePlatforms();
     requestAnimationFrame(update);
     document.addEventListener("keydown", moveBunny);
